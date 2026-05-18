bff253428549642d2c1c65a3f6f8ca3bb6b4a01c
Cristian Guerrero <cristian.guerrero-b@mail.escuelaing.edu.co>
2026-05-18 15:29:40 -0500

    Fix: Correcion Health

--- src/main/java/edu/eci/patricia/geolocalization/infrastructure/config/SecurityConfig.java
+++ src/main/java/edu/eci/patricia/geolocalization/infrastructure/config/SecurityConfig.java
@@ -45,6 +45,8 @@ public class SecurityConfig {
             .sessionManagement(s -> s.sessionCreationPolicy(SessionCreationPolicy.STATELESS))
             .authorizeHttpRequests(auth -> auth
                 .requestMatchers(
+                    "/actuator",
+                    "/actuator/**",
                     "/",
                     "/error",
                     "/swagger-ui/**",
